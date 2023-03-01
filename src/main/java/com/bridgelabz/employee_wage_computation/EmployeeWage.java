4ee4cf84f307c5826f3cee3e2306014d3f607284
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-03-01 16:41:49 +0530

    Refactored: refactord the code in class varibles and method

--- src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employee_wage_computation/EmployeeWage.java
@@ -1,28 +1,27 @@
 package com.bridgelabz.employee_wage_computation;
 
 public class EmployeeWage {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation program");
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int empWagePerHr = 20;
+    public static final int numOfWorkingDays = 20;
+    public static final int maximumHrsInMonth = 100;
+
+    public void employeeWage() {
         int empHrs;
-        int empWagePerHr = 20;
-        int numOfWorkingDays = 20;
-        int maximumHrsInMonth = 100;
         int totalEmployeeHrs = 0;
         int totalWorkingDays = 0;
         int totalEmployeeWage = 0;
         while (totalEmployeeHrs < maximumHrsInMonth && totalWorkingDays < numOfWorkingDays) {
             totalWorkingDays++;
             {
-
                 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
                 switch (empCheck) {
-                    case 1:
+                    case IS_FULL_TIME:
                         System.out.println("Employee is present");
                         empHrs = 8;
                         break;
-                    case 2:
+                    case IS_PART_TIME:
                         System.out.println("Employee half time present");
                         empHrs = 4;
                         break;
@@ -37,5 +36,13 @@ public class EmployeeWage {
 
         }
         System.out.println("Total employee wage " + totalEmployeeWage);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation program");
+
+        EmployeeWage employeeWage = new EmployeeWage();
+        employeeWage.employeeWage();
     }
 }
